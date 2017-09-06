8ce8ae838544097128e89c99c4b080c387af59af
Uilque Messias <messias@work.co>
2017-09-06 11:05:52 -0300

    Lesson 61 - Firebase client setup
    This commit does the Firebase client setup in auth project

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: '<YOUR_API_KEY_FROM_FIREBASE_SERVER>',
+            authDomain: '<YOUR_PROJECT_NAME_FROM_FIREBASE>.firebaseapp.com',
+            databaseURL: 'https://<YOUR_PROJECT_NAME_FROM_FIREBASE>.firebaseio.com',
+            projectId: '<YOUR_PROJECT_NAME_FROM_FIREBASE>',
+            storageBucket: '<YOUR_PROJECT_NAME_FROM_FIREBASE>.appspot.com',
+            messagingSenderId: '<YOUR_MESSAGING_SENDER_ID_FROM_FIREBASE>'
+        });
+    }
+
     render() {
         return (
             <View>
