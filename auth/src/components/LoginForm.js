8fa2ae95afb339fb1dcea528648a1966fe5dda36
Uilque Messias <messias@work.co>
2017-09-07 12:33:42 -0300

    Lesson 72 - Error handling
    This commit adds an error handling on login/sign up flow.

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Button, Card, CardSection, Input } from './common';
 import firebase from 'firebase';
 
 class LoginForm extends Component {
-    state = { email: '', password: '' }
+    state = { email: '', password: '', error: '' }
 
     onButtonPress = () => {
         const { email, password } = this.state;
-        firebase.auth().signInWithEmailAndPassword(email, password);
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .catch(() => {
+                        this.setState({ error: 'Authentication failed.' })
+                    });
+            });
     }
 
     render() {
@@ -30,6 +37,11 @@ class LoginForm extends Component {
                         onChangeText={password => this.setState({ password })}
                     />
                 </CardSection>
+                
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
                     <Button onPress={this.onButtonPress}>
                         Log in
@@ -39,5 +51,13 @@ class LoginForm extends Component {
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red',
+    },
+};
 
 export default LoginForm;
