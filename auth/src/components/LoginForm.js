de0ec550d3397a21682f05e619ffc1431760fd10
Uilque Messias <messias@work.co>
2017-09-07 12:58:54 -0300

    Lesson 73 - More on authentication flow
    This commit a rule to show error and creates a new
    `Spinner` component.

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -8,6 +8,9 @@ class LoginForm extends Component {
 
     onButtonPress = () => {
         const { email, password } = this.state;
+
+        this.setState({ error: '' });
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password)
