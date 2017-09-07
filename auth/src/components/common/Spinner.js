b9573a0a49253bde34fc066583b3a0160e378f08
Uilque Messias <messias@work.co>
2017-09-07 13:05:12 -0300

    Lesson 74 - Creating an activity spinner
    This commit creates the real ui for `Spinner` using an
    `ActivityIndicator` component.

--- auth/src/components/common/Spinner.js
+++ auth/src/components/common/Spinner.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => (
-    <View />
+const Spinner = ({ size }) => (
+    <View style={styles.spinnerStyle}>
+        <ActivityIndicator size={size || 'large'} />
+    </View>
 );
+
+const styles = {
+    spinnerStyle: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+};
 
 export { Spinner };
