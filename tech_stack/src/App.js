2050b66a0534c121999e8f5515cc3150d2fef313
Uilque Messias <messias@work.co>
2017-09-07 17:33:49 -0300

    Lesson 85 - More on redux boilerplate
    This commit start to use `Redux` and Wrapup the `App` component with
    `Provider` component.

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 
-const App = () => {
-    return (
+const App = () => (
+    <Provider store={createStore(reducers)}>
         <View />
-    );
-};
+    </Provider>
+);
 
 export default App;
