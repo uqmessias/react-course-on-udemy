d82f328a9f47b114e200c285804afd771e073948
Uilque Messias <messias@work.co>
2017-09-11 18:26:04 -0300

    Lesson 100 - Adding a touchable
    This commit adds a `TouchableWithoutFeedback` component into `ListItem`
    component in order to add a `onPress` feedback to each item of the list.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common/CardSection';
 import * as actions from '../actions';
@@ -7,14 +7,21 @@ import * as actions from '../actions';
 class ListItem extends Component {
     render() {
         const { titleStyle } = styles;
+        const { id, title } = this.props.library;
 
         console.log(this.props);
         return (
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}>
+                            {title}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
