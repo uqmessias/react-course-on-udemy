74e91050b3cc59a8a2bc88a14c64ebcb253252f3
Uilque Messias <messias@work.co>
2017-09-11 18:39:12 -0300

    Lesson 102 - Expanding a row
    This commit adds a rule to expand the library description when it's
    clicked.

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -5,6 +5,18 @@ import { CardSection } from './common/CardSection';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    renderDescription = () => {
+        const { library, selectedLibraryId } = this.props;
+
+        if (library.id === selectedLibraryId) {
+            return (
+                <Text>
+                    {library.description}
+                </Text>
+            );
+        }
+    }
+
     render() {
         const { titleStyle } = styles;
         const { id, title } = this.props.library;
@@ -20,6 +32,7 @@ class ListItem extends Component {
                             {title}
                         </Text>
                     </CardSection>
+                    {this.renderDescription()}
                 </View>
             </TouchableWithoutFeedback>
         );
@@ -33,4 +46,8 @@ const styles = {
     },
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => ({
+    selectedLibraryId: state.selectedLibraryId,
+});
+
+export default connect(mapStateToProps, actions)(ListItem);
