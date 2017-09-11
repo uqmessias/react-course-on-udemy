053b85523363f771da4af75a75e5da0e3ec91f8b
Uilque Messias <messias@work.co>
2017-09-11 17:07:26 -0300

    Lesson 91 - MapStateToProps with connect
    This commit shows the use of the first argument of the `connect` function
    (`mapStateToProps` function).

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => (
     <Provider store={createStore(reducers)}>
         <View>
             <Header headerText="Tech Stack" />
+            <LibraryList />
         </View>
     </Provider>
 );
