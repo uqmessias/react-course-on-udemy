c526897a466728b375712118e890884f9e65c2c5
Uilque Messias <messias@work.co>
2017-09-12 11:25:31 -0300

    Lesson 109 - More on boilerplate setup
    This commit adds and configures the `firebase` module
    and creates the first reducer of the application.

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+    componentWillMount() {
+        const config = {
+            apiKey: '<YOUR_API_KEY_FROM_FIREBASE_SERVER>',
+            authDomain: '<YOUR_PROJECT_NAME_FROM_FIREBASE>.firebaseapp.com',
+            databaseURL: 'https://<YOUR_PROJECT_NAME_FROM_FIREBASE>.firebaseio.com',
+            projectId: '<YOUR_PROJECT_NAME_FROM_FIREBASE>',
+            storageBucket: '',
+            messagingSenderId: '<YOUR_MESSAGING_SENDER_ID_FROM_FIREBASE>'
+        };
+
+        firebase.initializeApp(config);
+    }
     render() {
         return (
-            <Provider store={createStore()}>
+            <Provider store={createStore(reducers)}>
                 <View>
                     <Text>
                         Hello!
