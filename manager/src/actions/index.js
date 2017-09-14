ac424687cde0d7055bd8c52493dec978a7025095
Uilque Messias <messias@work.co>
2017-09-14 14:50:18 -0300

    Lesson 120 - Redux thunk in practice
    This commit applies the `redux-thunk` as a middleware to Redux
    store.

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -15,7 +15,7 @@ export const passwordChanged = text => ({
     payload: text,
 });
 
-export const loginUser = ({ email, password }) => {
+export const loginUser = ({ email, password }) => (dispatch => {
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => console.log(user));
-};
+});
