0cbbc2c399eb0f6d9eb732fa578ff7b8ea24d9a6
Uilque Messias <messias@work.co>
2017-09-19 12:12:36 -0300

    Lesson 122 - Making LoginUser more robust
    This commit changes the hard-coded string type to a constant
    string and adds the `user` property into the global state.

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -3,6 +3,7 @@ import firebase from 'firebase';
 import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
 } from './types';
 
 export const emailChanged = text => ({
@@ -18,6 +19,6 @@ export const passwordChanged = text => ({
 export const loginUser = ({ email, password }) => (dispatch => {
     firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => {
-            dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+            dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
         });
 });
