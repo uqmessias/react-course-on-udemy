1d3854a3fa876f108ed450be42ca0bab07a4ab37
Uilque Messias <messias@work.co>
2017-09-19 14:01:44 -0300

    lesson 125 - A Firebase gotcha
    This commit just removes the error message in case of a success right
    after an error.

--- manager/src/reducers/authReducer.js
+++ manager/src/reducers/authReducer.js
@@ -28,6 +28,7 @@ export default (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 user: action.payload,
+                error: '',
             };
         case LOGIN_USER_FAIL:
             return {
