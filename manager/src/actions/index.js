5e1b4ef840112de638c670142aecaecd4fffae62
Uilque Messias <messias@work.co>
2017-09-19 13:37:07 -0300

    Lesson 123 - Creating user accounts
    This commit adds some logic to handle the login even when the
    user doesn't exist and tries to create that user.

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -4,6 +4,7 @@ import {
     EMAIL_CHANGED,
     PASSWORD_CHANGED,
     LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL,
 } from './types';
 
 export const emailChanged = text => ({
@@ -18,7 +19,21 @@ export const passwordChanged = text => ({
 
 export const loginUser = ({ email, password }) => (dispatch => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(user => {
-            dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(user => loginUserSuccess(dispatch, user))
+                .catch(() => loginUserFail(dispatch));
         });
 });
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user,
+    });
+};
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
+};
