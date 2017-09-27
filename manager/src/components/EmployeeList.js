2a03f762a38ec4b9a0566876f091de20f2dc3c8d
Uilque Messias <messias@work.co>
2017-09-27 18:45:45 -0300

    Lesson 149 - Dynamic datasource building
    This commit creates a datasource with the current
    `employees` piece of state and when it changes, it recreates
    the datasource.

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
+import { View, Text, ListView } from 'react-native';
 import { employeeFetch } from '../actions';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeeFetch();
+        this.createDataSource(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.createDataSource(nextProps);
+    }
+
+    createDataSource({ employees }) {
+        const ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.dataSource = ds.cloneWithRows(employees);
     }
 
     render() {
