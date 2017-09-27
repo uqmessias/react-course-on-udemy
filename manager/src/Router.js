620666223146bd691f3032aa6035a4333581668c
Uilque Messias <messias@work.co>
2017-09-27 15:15:29 -0300

    Lesson 134 - Navigating to the employee creation form
    This commit adds the new `EmployeeCreate` component and
    updates the `Router` to allow the user navigating to `EmployeeCreate`.

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -1,32 +1,36 @@
 import React from 'react';
-import { Stack, Modal, Scene, Router } from 'react-native-router-flux';
+import { Stack, Modal, Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeCreate from './components/EmployeeCreate';
 
-const RouterComponent = () => {
-    return (
-        <Router>
-            <Modal hideNavBar>
-                <Scene key="auth">
-                    <Scene
-                        key="login"
-                        component={LoginForm}
-                        title="Please login"
-                    />
-                </Scene>
-                <Stack key="main" >
-                    <Scene
-                        rightTitle="Add"
-                        onRight={(obj) => console.log('onright: ', obj)}
-                        key="employeeList"
-                        component={EmployeeList}
-                        titleStyle={{ alignSelf: 'center' }}
-                        title="Employees"
-                    />
-                </Stack>
-            </Modal>
-        </Router>
-    );
-};
+const RouterComponent = () => (
+    <Router>
+        <Modal hideNavBar>
+            <Scene key="auth">
+                <Scene
+                    key="login"
+                    component={LoginForm}
+                    title="Please login"
+                />
+            </Scene>
+            <Stack key="main" >
+                <Scene
+                    rightTitle="Add"
+                    onRight={() => Actions.employeeCreate()}
+                    key="employeeList"
+                    component={EmployeeList}
+                    title="Employees"
+                    initial
+                />
+                <Scene
+                    key="employeeCreate"
+                    component={EmployeCreate}
+                    title="Create Employee"
+                />
+            </Stack>
+        </Modal>
+    </Router>
+);
 
 export default RouterComponent;
