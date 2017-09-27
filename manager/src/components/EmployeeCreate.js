856ec13d4ca447fcd4ba2ef000f0d983c92fc392
Uilque Messias <messias@work.co>
2017-09-27 15:29:50 -0300

    Lesson 135 - Building the Employee creation form

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
     render() {
         return (
-            <View>
-                <Text>Employee form</Text>
-            </View>
+            <Card>
+                <CardSection>
+                    <Input
+                        label="Name"
+                        placeholder="Jane"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label="Phone"
+                        placeholder="(11) 9 8765 4321"
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Create
+                    </Button>
+                </CardSection>
+            </Card>
         );
     }
 }
