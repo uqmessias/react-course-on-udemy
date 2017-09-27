3aae45d5e2610888dd4c61fadb2b171e8ebdfe92
Uilque Messias <messias@work.co>
2017-09-27 15:36:55 -0300

    Lesson 136 - Employee form actions
    This commit splits the employee form action creators
    from the auth's ones.

---
+++ manager/src/reducers/employeeFormReducer.js
@@ -0,0 +1,13 @@
+import {
+    EMPLOYEE_UPDATE,
+} from '../actions/types';
+
+const INITIAL_STATE = {};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case EMPLOYEE_UPDATE:
+        default:
+            return state;
+    }
+};
