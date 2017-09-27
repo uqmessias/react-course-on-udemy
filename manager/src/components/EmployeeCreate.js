76cf50cf75d3c48b9ba655429bb7784d502468dd
Uilque Messias <messias@work.co>
2017-09-27 17:05:59 -0300

    Lesson 144 - Default form values
    This commit just change the default value of **shift** to "**Monday**"

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -7,7 +7,7 @@ import { Card, CardSection, Input, Button } from './common';
 class EmployeeCreate extends Component {
     onButtonPress = () => {
         const { name, phone, shift } = this.props;
-        this.props.employeeCreate({ name, phone, shift });
+        this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
     }
 
     render() {
